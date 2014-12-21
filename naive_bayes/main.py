6f952d4684edda06abc2e7262ff37c9a0ab96606
cjackie <chaojie.keepgoing@gmail.com>
2014-12-21 00:04:46 -0500

    skeleton

---
+++ naive_bayes/main.py
@@ -0,0 +1,5 @@
+import sys
+
+if __name__ == "__main__":
+    #TODO
+    #setup the command line interface
---
+++ naive_bayes/res/naive_bayes.py
@@ -0,0 +1,27 @@
+#############################################################
+# NaiveBayse, the class implement the naive bayse algorithm.#
+#############################################################
+
+class NaiveBayse:
+    
+    def __init__(self, data_path):
+        #TODO construct the model
+    
+    """
+    parse the file and return processable data
+    @data_path: the absolute path to the file
+    @return: an array of dictionary. The key of dictionary is type number
+             and the value is an array of numbers
+    """
+    def parse(data_path):
+        #TODO
+    
+    """
+    predict the result given an input
+    @x: an input feature vector.
+    @return: return 1 or 0
+    """
+    def predict(x):
+        #TODO
+        
+    
