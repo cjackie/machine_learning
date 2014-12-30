257dcd56d5a36608506c19efad0b71ed5edc0769
Chaojie Wang <chaojie.keepgoing@gmail.com>
2014-12-30 18:48:24 -0500

    escape newline character

--- logistic_regression/main.py
+++ logistic_regression/main.py
@@ -3,9 +3,9 @@ import logistic_reg.log_reg.LogReg
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("usage:python main.py TRAINING_DATA_PATH 
+        print("usage:python main.py TRAINING_DATA_PATH \
                TEST_DATA_PATH OPTIMIZATION_CODE ALPHA SHOW_PROGRESS")
-        print("consult README file details about optimization code, alpha
+        print("consult README file details about optimization code, alpha \
                and show_progress")
         sys.exit(1)
 
