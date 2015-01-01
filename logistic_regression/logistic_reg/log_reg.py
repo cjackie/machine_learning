533b69f7625d05ec7a4e32a8e57d489901e3fca3
cjackie <chaojie.keepgoing@gmail.com>
2014-12-31 22:27:42 -0500

    complete model and interface

--- logistic_regression/logistic_reg/log_reg.py
+++ logistic_regression/logistic_reg/log_reg.py
@@ -1,5 +1,8 @@
 import numpy as np
 import scipy.linalg as linalg
+import numpy.linalg.norm
+from util import parse
+from time import clock
 
 class LogReg:
     
@@ -7,27 +10,20 @@ class LogReg:
     GRADIENT_ASCENT = 'grad'
     NEWTON_METHOD = 'newton'
     
-    def __init__(self, data_path, asc_deg=0.1,
+    def __init__(self, data_path, resolution=0.1**4, alpha=0.1
                  opt_code=STOCHASTIC_GRADIENT_ASCENT, show_prog=False):
-        #TODO contruct the model
-
-    """
-    parse the data and return result vector and the data matrix, assume 
-    data format is csv with first column being output
-    @data_path: string, the absolute data path to the data
-    @return, (y, X), both type np.matrix, y is a vector, X is matrix with 
-             row being each data, and column being features
-    """
-    def parse(self, data_path):
-        #TODO
-
+        (names, y, X) = parse(data_path)
+        self._show_prog = show_prog
+        self._theta = self.__compute_parameters(y, X, opt_code, alpha, resolution)
+        
     """
     predict the result by the model
     @x, np.matrix, a vector
     @return, float, the result
     """
     def predict(self, x):
-        #TODO
+        e = np.e
+        return 1.0/(1+e**(-(self._theta.T*x).item(0,0)))
         
     """
     choose the corresponding method accorrding to opt_code
@@ -35,40 +31,89 @@ class LogReg:
     @X, np.matrix, X is matrix with row being each data, and column being features
     @opt_code, string, to indicate which optimization algorithm to run
     @alpha, float, "step size" for optimization algorithm
+    @resolution, float, accepted converging number
     @return, np.matrix, a vector for parameter theta
     """
-    def __compute_parameters(self, y, X, opt_code, alpha):
-        #TODO
+    def __compute_parameters(self, y, X, opt_code, alpha, resolution):
+        if opt_code == STOCHASTIC_GRADIENT_ASCENT:
+            return self.__stoch(y, X, alpha, resolution)
+        elif opt_code == GRADIENT_ASCENT:
+            return self.__grad(y, X, alpha, resolution)
+        elif opt_code == NEWTON_METHOD:
+            return self.__newton(y, X, alpha, resolution)
+        else:
+            print("invalid opt_code!!, exiting")
+            exit(-1)
 
     """
     stochastic gradient ascent
     @y, np.matrix, a vector
     @X, np.matrix, X is matrix with row being each data, and column being features
-    @alpha, float, "step size" for optimization algorithm
+    @alpha, float, "step size" for optimization algorithm    
+    @resolution, float, accepted converging number
     @return, np.matrix, a vector for parameter theta
     """
-    def __stoch(self, y, X, alpha):
-        #TODO
+    def __stoch(self, y, X, alpha, resolution):
+        theta = np.matrix([0]*len(X.T)).T
+        h = lambda (x, theta): 1.0/(1+np.e**(-(self._theta.T*x).item(0,0)))    #model
+        if self._show_prog:
+            before = clock()
+            iter_c = 1
+        for i in range(len(X.A)):
+            x = np.matrix(X.A[i]).T
+            for j in range(len(x)):
+                delta = alpha*(y.A[i][0] - h(x, theta))*x
+                theta += delta
+                if self._show_prog:
+                    print("for row %d: %s\n\
+                           increase by: %s\n\
+                           theta after updating: %s" \
+                           % (iter_c,X.A[i],delta.flatten().A[0],theta.flatten().A[0]));
+                    iter_c += 1
+        if self._show_prog:
+            print("time spending on optimization: %f", clock()-before)
+        self._theta = theta
         
     """
     gradient ascent
     @y, np.matrix, a vector
     @X, np.matrix, X is matrix with row being each data, and column being features
     @alpha, float, "step size" for optimization algorithm
+    @resolution, float, accepted converging number
     @return, np.matrix, a vector for parameter theta
     """
-    def __grad(self, y, X, alpha):
-        #TODO
+    def __grad(self, y, X, alpha, resolution):
+        #return a function to compute gradient vector
+        def gradient(y, X):
+            def g(theta):
+                m = np.matrix
+                h = lambda (x, theta): 1.0/(1+np.e**(-(self._theta.T*x).item(0,0)))    #model
+                h_v = m([h(m(x).T, theta) for x in X.A]).T
+                return X.T*(y-h_v)
+            return g
+        
+        g = gradient(y,X)
+        theta = np.matrix([0]*len(X.T)).T
+        while True:
+            g_v = g(theta)
+            theta += alpha*g_v
+            if self._show_prog:
+                print("gradient: %s, theta is: %s", (g_v.flatten().A[0], theta.flatten().A[0]))
+            if (norm(g_v) < resolution):
+                return theta
         
     """
     newton's method
     @y, np.matrix, a vector
     @X, np.matrix, X is matrix with row being each data, and column being features
     @alpha, float, "step size" for optimization algorithm
+    @resolution, float, accepted converging number
     @return, np.matrix, a vector for parameter theta
     """
-    def __newton(self, y, X, alpha):
+    def __newton(self, y, X, alpha, resolution):
         #TODO
         
-        
+    @property
+    def theta(self):
+        return np.matrix(self._theta)
         
--- logistic_regression/main.py
+++ logistic_regression/main.py
@@ -1,10 +1,11 @@
 import sys
 import logistic_reg.log_reg.LogReg
+from logistic_reg.util import parse
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
+    if len(sys.argv) != 7:
         print("usage:python main.py TRAINING_DATA_PATH \
-               TEST_DATA_PATH OPTIMIZATION_CODE ALPHA SHOW_PROGRESS")
+               TEST_DATA_PATH OPTIMIZATION_CODE ALPHA RESOLUTION SHOW_PROGRESS")
         print("consult README file details about optimization code, alpha \
                and show_progress")
         sys.exit(1)
@@ -13,4 +14,31 @@ if __name__ == "__main__":
     test_file = sys.argv[2]
     optimiztion = int(sys.argv[3])
     alpha = float(sys.argv[4])
-    #TODO
+    resolution = float(sys.argv[5])
+    show_prog = True if sys.argv[6]=='1' else False
+    opt_code = 0
+    if optimization == 0:
+        opt_code=LogReg.STOCHASTIC_GRADIENT_ASCENT
+    elif optimization == 1:
+        opt_code=LogReg.GRADIENT_ASCENT
+    elif optimization == 2:
+        opt_code=LogReg.NEWTON_METHOD
+    model = LogReg(training_file, resolution, alpha, opt_code, show_prog)
+
+    (names, y, X) = parse(test_file)
+    (correct_counts, incorrect_counts) = (0,0)
+    for i in len(X.A):
+        ans = model.predict(np.matrix(X.A[i]).T)
+        if ans>0.5:
+            correct_counts += 1 if y.A[i][0]==1 else 0
+            incorrect_counts += 1 if y.A[i][0]==0 else 0
+        else:
+            correct_counts += 1 if y.A[i][0]==0 else 0
+            incorrect_counts += 1 if y.A[i][0]==1 else 0
+
+    print("%d%% of the data were corrected classified!" % \
+          float(correct_counts)/(correct_counts+incorrect_counts))
+    
+        
+        
+        
